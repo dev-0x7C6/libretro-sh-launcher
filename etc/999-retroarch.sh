efd623c89d8e8bf8cf21495c16ccc7deb36fd220
VolkMilit <javirrdar@gmail.com>
2018-03-23 17:11:17 +0800

    fix eudev 220 bug, RA doesn't seen gamepad

--- etc/999-retroarch.sh
+++ etc/999-retroarch.sh
@@ -3,8 +3,4 @@
 export HOME=/home/user
 export DISPLAY=:0 
 
-/usr/bin/retroarch -L /usr/lib/x86_64-linux-gnu/libretro/sh_launcher_libretro.so &
-
-sleep 1
-
-kill -12 $(pidof retroarch | cut -d' ' -f1) # there is two instance of retroarch, and I can't fix that :/
+/usr/bin/retroarch -L /usr/lib/x86_64-linux-gnu/libretro/sh_launcher_libretro.so >> /dev/null 2>&1 &
